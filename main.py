c72a81f7f82030421d14b0385e6758f3b672f90f
Mohamed Nishath M <78273608+nich-nichy@users.noreply.github.com>
2024-11-07 13:18:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1064,3 +1064,22 @@ arr = "A23 B56 B56 C79 D16"
 print(passportNumbers(arr, n))
 
 
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not self.isAlphaNum(s[l]):
+                l += 1
+            while r > l and not self.isAlphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        return True
+    
+    def isAlphaNum(self, word):
+        return (ord('A') <= ord(word) <= ord('Z') or
+        ord('a') <= ord(word) <= ord('z') or
+        ord('0') <= ord(word) <= ord('9')
+        )
+
