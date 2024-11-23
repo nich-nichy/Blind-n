3867a10945454c6dc89eec802a9e436b1b918ff8
Mohamed Nishath M <78273608+nich-nichy@users.noreply.github.com>
2024-11-23 12:15:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1157,3 +1157,19 @@ def threeSum(nums):
 nums = [-1,0,1,2,-1,-4]
 print(threeSum(nums))
 
+def maxArea(height):
+    res = 0
+    l, r = 0, len(height) - 1
+
+    while l < r:
+        area = (r - l) * min(height[l], height[r])
+        res = max(res, area)
+        if height[l] < height[r]:
+            l += 1
+        else:
+            r -= 1
+    return res
+
+height = [1,8,6,2,5,4,8,3,7]
+print(maxArea(height))
+
