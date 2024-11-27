123e7517633291bfd24e28866530010f2fbc05fd
Mohamed Nishath M <78273608+nich-nichy@users.noreply.github.com>
2024-11-27 13:30:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1233,3 +1233,35 @@ print(lengthOfLongestSubstring(s1))
 print(lengthOfLongestSubstring(s2))
 print(lengthOfLongestSubstring(s3))
 
+def minWindow(s, t):
+        if t == "": return ""
+
+        countT, window = {}, {}
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+
+        have, need = 0, len(countT)
+        res, resLen = [-1, -1], float("infinity")
+        l = 0
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+
+            if c in countT and window[c] == countT[c]:
+                have += 1
+
+            while have == need:
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    resLen = (r - l + 1)
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+        l, r = res
+        return s[l : r + 1] if res != float("infinity") else ""
+
+s = "ADOBECODEBANC"
+t = "ABC"
+print(minWindow(s, t))
+
