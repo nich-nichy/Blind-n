71665c971f8e99793a619d73171c0770469a1e3c
Mohamed Nishath M <78273608+nich-nichy@users.noreply.github.com>
2024-11-28 22:27:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1265,3 +1265,35 @@ s = "ADOBECODEBANC"
 t = "ABC"
 print(minWindow(s, t))
 
+def minWindow(s, t):
+        if t == "": return ""
+        countT, window = {}, {} # hash maps
+
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+
+        have, seen = 0, len(countT)
+        l = 0
+        res, resLen = [-1, -1], float("infinity")
+
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+
+            if c in countT and countT[c] == window[c]:
+                    have += 1
+
+            while have == seen:
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    resLen = (r - l + 1)
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+        l, r = res
+        return s[l:r+1] if resLen != float("infinity") else ""
+
+s = "ADOBECODEBANC"
+t = "ABC"
+print(minWindow(s, t))
