11a12df742a70e1bf0934977c8ccd9cba5ad4071
Mohamed Nishath M <78273608+nich-nichy@users.noreply.github.com>
2024-12-01 22:22:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1337,3 +1337,31 @@ def findMin(arr):
 # print(findMin([3,4,5,1,2])) # 1
 print(findMin([4,5,6,7,0,1,2])) # 0
 # print(findMin([11,13,15,17])) # 11
+
+
+
+def search(arr, target):
+    l, r = 0, len(arr) - 1
+
+    while l <= r:
+        mid = (l + r) // 2
+
+        if arr[mid] == target:
+            return mid
+
+        if arr[mid] >= arr[l]:
+            if target >= arr[mid] or target < arr[l]:
+                l = mid + 1
+            else:
+                r = mid - 1
+        else:
+            if target >= arr[mid]:
+                l = mid - 1
+            else:
+                r = mid + 1
+    return -1
+
+target = 2
+print(search([4,5,6,7,0,1,2], target)) # 4
+
+
