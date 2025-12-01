cb8ede5877c74be20bd61efef7832a62a3ad2361
nich-nichy <nishathmohamed786@gmail.com>
2025-12-01 18:25:23 +0530

    Top k freq element

---
+++ learning.py
@@ -0,0 +1,19 @@
+def topKElements(arr, k):
+    count = {}
+    freq = [[] for i in range(len(arr))]
+    for i in arr:
+        count[i] = 1 + count.get(i, 0)
+    for n, c in count.items():
+        freq[c].append(n)
+    res = []
+    for n in range(len(arr) - 1, 0, -1):
+        print(n, "n", freq[n])
+        for v in freq[n]:
+            res.append(v)
+            if len(res) == k:
+                return res
+
+
+arr = [1, 1, 1, 2, 2, 100]
+k = 2
+print(topKElements(arr, k))
