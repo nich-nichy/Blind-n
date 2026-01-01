a4f2fbf70dc38c58707f7a1769cbd69f9cf4dc79
Mohamed Nishath M <78273608+nich-nichy@users.noreply.github.com>
2026-01-01 08:41:33 +0530

    Add minWindow function for substring search
    Implement minWindow function to find minimum window substring.

--- main.py
+++ main.py
@@ -1951,3 +1951,33 @@ def isValid(s):
             else:
                 stack.append(c)
         return True if not stack else False
+
+
+def minWindow(self, s: str, t: str) -> str:
+        if t == "":
+            return ""
+        window, countT = {}, {}
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+        
+        have, seen = 0, len(countT)
+        res, resLen = [-1, -1], float("infinity")
+        l = 0
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+
+            if c in countT and window[c] == countT[c]:
+                have += 1
+
+            while have == seen:
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    resLen = r - l + 1
+                window[s[l]] -= 1
+
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+        l, r = res
+        return s[l : r + 1] if resLen != float("infinity") else ""
