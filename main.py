3d5b4ecb051c614287a938541bc672350495dd04
Mohamed Nishath M <78273608+nich-nichy@users.noreply.github.com>
2026-01-03 08:26:41 +0530

    Add findMin function to find minimum in rotated array

--- main.py
+++ main.py
@@ -1981,3 +1981,19 @@ def minWindow(self, s: str, t: str) -> str:
                 l += 1
         l, r = res
         return s[l : r + 1] if resLen != float("infinity") else ""
+
+
+def findMin(self, nums: List[int]) -> int:
+        res = nums[0]
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            if nums[l] < nums[r]:
+                res = min(res, nums[l])
+                break
+            mid = (l + r) // 2
+            res = min(res, nums[mid])
+            if nums[mid] >= nums[l]:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return res
